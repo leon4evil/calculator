a19ef1a1434e299f87d9e7da9afc99f118a7065b
Luis Hierro <tuilli2@hotmail.com>
2022-12-10 22:20:05 -0800

    added acceptance test stage and post section

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,2 +1,2 @@
 #!/bin/zsh
-test $(curl --retry-connrefused --retry 1 --retry-delay 1 '127.0.0.1:88/sum?a=1&b=4') -eq 5
+test $(curl --retry-connrefused --retry 1 --retry-delay 1 'host.docker.internal:88/sum?a=1&b=4') -eq 5
