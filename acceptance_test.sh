eb48494438c2e073afc577336fe68ef2ccd059fa
Luis Hierro <tuilli2@hotmail.com>
2022-12-10 20:03:29 -0800

    added acceptance test stage and post section

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-test $(curl --retry-connrefused --retry 3 --retry-delay 3 'http://127.0.0.1:88/sum?a=1&b=4') -eq 5
+test $(curl --retry-connrefused --retry 3 --retry-delay 3 'https://www.google.com') -eq 5
