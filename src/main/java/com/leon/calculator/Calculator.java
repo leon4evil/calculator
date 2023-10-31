b8294419b5f18cf2aebc2a7b61879792f322896a
Luis Hierro <tuilli2@hotmail.com>
2023-10-30 20:46:26 -0700

    fixed that tests not running issue by commenting out useJUnitPlatform()

--- src/main/java/com/leon/calculator/Calculator.java
+++ src/main/java/com/leon/calculator/Calculator.java
@@ -1,17 +1,17 @@
 package com.leon.calculator;
-import org.springframework.cache.annotation.Cacheable;
+//import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class Calculator {
-    @Cacheable("sum")
+    //@Cacheable("sum")
     public int sum(int a, int b) {
-        try {
-            Thread.sleep(3000);
-        }
-        catch (InterruptedException e){
-            e.printStackTrace();
-        }
+//        try {
+//            Thread.sleep(3000);
+//        }
+//        catch (InterruptedException e){
+//            e.printStackTrace();
+//        }
         return a + b;
     }
 }
