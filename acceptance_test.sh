d94547d3167f05d3dd2e8dfe0e9936b66d951a91
Luis Hierro <tuilli2@hotmail.com>
2023-11-12 15:35:42 -0800

    corrected acceptance_test.sh

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -4,4 +4,4 @@ set -x
 
 # NODE_IP=$(kubectl get nodes -o jsonpath='{ $.items[0].status.addresses[? (@.type==ExternalIP)].address }')
 NODE_PORT=$(kubectl get svc calculator-service -o jsonpath='{.spec.ports[0].nodePort}')
-./gradlew acceptsanceTest -Dcalculator.url=http://host.docker.internal:${NODE_PORT}
+./gradlew acceptanceTest -Dcalculator.url=http://host.docker.internal:${NODE_PORT}
