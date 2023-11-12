d3cd9e379561a2309f54b56159102dcf9c4b11bd
Luis Hierro <tuilli2@hotmail.com>
2023-11-12 15:29:47 -0800

    corrected acceptance_test.sh

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-test $(curl --retry-connrefused --retry 5 --retry-delay 1 'host.docker.internal:88/sum?a=1&b=4') -eq 5
+# test $(curl --retry-connrefused --retry 5 --retry-delay 1 'host.docker.internal:88/sum?a=1&b=4') -eq 5
+set -x
+
+# NODE_IP=$(kubectl get nodes -o jsonpath='{ $.items[0].status.addresses[? (@.type==ExternalIP)].address }')
+NODE_PORT=$(kubectl get svc calculator-service -o jsonpath='{.spec.ports[0].nodePort}')
+./gradlew acceptsanceTest -Dcalculator.url=http://host.docker.internal:${NODE_PORT}
